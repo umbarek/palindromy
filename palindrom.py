4f3788632b77b4b2edaa346dfaf6ea12e595d9ff
Tomasz Pograniczny <tpograniczny@gmail.com>
2022-09-02 12:54:23 +0200

    Add palindrome check function

--- palindrom.py
+++ palindrom.py
@@ -4,3 +4,7 @@ n = input("Podaj słowo do sprawdzenia: ")
 print(f"Podane przez Ciebie słowo to:" + "\033[1m",n)
 print ("\033[0m")
 
+def palindromy(word):
+    word = word.lower()
+    reverse_word = "".join(reversed(word))
+
